5fd9cb780c315095089abab5ad1bb936f3d9b2bd
Lucas-Tyman <99354994+Lucas-Tyman@users.noreply.github.com>
2022-04-06 17:15:03 +0000

    changes

--- js/script.js
+++ js/script.js
@@ -3,7 +3,17 @@
 // This file contains the JS functions for index.html
 
 'use strict'
+
 /**
- * This function calculates 
+ * This function converts fahrenheit to celsius
  */
-function calculate()
+function calculate() {
+  // input
+  const fahrenheit = parseFloat(document.getElementById('fahrenheit').value)
+
+  // process
+  const celsius = (fahrenheit - 32) * 5 / 9
+
+  // output
+  document.getElementById('conversion').innerHTML = fahrenheit + '°F converted to celsius '  + celsius.toFixed(2) + ' °C'
+}
